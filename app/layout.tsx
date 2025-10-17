b5de295d71521ac98929cb41376e27c02fa40861
Nicolasb03 <nicolas.bedard03@gmail.com>
2025-10-17 11:51:37 +0300

    Add Google Tag Manager integration (GTM-KJBTB7MG)

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,21 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KJBTB7MG');
+            `
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
@@ -126,6 +141,17 @@ export default function RootLayout({
         {/* End Meta Pixel Code */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJBTB7MG"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <LanguageProvider>{children}</LanguageProvider>
         <Analytics />
       </body>
