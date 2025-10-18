6623a10f2ee8de0fe497144e5f26de2466db1893
Nicolasb03 <nicolas.bedard03@gmail.com>
2025-10-18 04:07:15 +0300

    Update Meta Pixel ID to 1516202722651792

--- app/layout.tsx
+++ app/layout.tsx
@@ -139,7 +139,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '952896926960917');
+              fbq('init', '1516202722651792');
               fbq('track', 'PageView');
             `
           }}
@@ -149,7 +149,7 @@ export default function RootLayout({
             height="1" 
             width="1" 
             style={{display:'none'}}
-            src="https://www.facebook.com/tr?id=952896926960917&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1516202722651792&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
